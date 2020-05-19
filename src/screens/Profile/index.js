72a3b4f19b4690a1077e699e0690cb0b437e520f
bruno <bruno@iafox.com>
2020-05-19 14:43:44 -0300

    Image Picker

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, Image } from 'react-native';
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
 import styles from './styles';
 
 const Profile = () => {
+  const [image, setImage] = useState(null);
+
+  const handleSetImage = (image) => {
+    setImage(image);
+  }
+
+  const _pickImage = async () => {
+    try {
+      await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        quality: 1
+      });
+
+      if(!result.cancelled) {
+        handleSetImage(result.uri);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   let [fontsLoaded] = useFonts({
     'FrancoisOne': require('../../../assets/fonts/FrancoisOne-Regular.ttf'),
     'Jost': require('../../../assets/fonts/Jost.ttf')
@@ -18,11 +44,17 @@ const Profile = () => {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity style={styles.photoMoldure}>
-            <FontAwesome name="user-circle" size={160} color='#CCC'/>
-            <View style={styles.cameraIcon}>
-              <FontAwesome name="camera-retro" size={25} color="#FFF"/>
-            </View>
+          <TouchableOpacity style={styles.photoMoldure} onPress={_pickImage}>
+            {image ? (
+              <Image source={{uri: image}} style={styles.photoMoldure}/>
+            ) : (
+              <>
+                <FontAwesome name="user-circle" size={160} color='#CCC'/>
+                <View style={styles.cameraIcon}>
+                  <FontAwesome name="camera-retro" size={25} color="#FFF"/>
+                </View>
+              </>
+            )}
           </TouchableOpacity>
         </View>
         <View style={styles.headerName}>
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -15,7 +15,15 @@ export default StyleSheet.create({
     width: 160,
     height: 160,
     borderRadius: 80,
-    backgroundColor: '#FFF'
+    backgroundColor: '#FFF',
+    shadowColor: "#000",
+    shadowOffset: {
+    	width: 0,
+    	height: 6,
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.30, 
+    elevation: 13,
   },
   cameraIcon: {
     position: 'absolute',
